332d6cdfe79aeff0a9c0c6dacf6e97ee1395e4d7
Jsson47 <martinezvalle98@gmail.com>
2025-03-24 21:33:02 -0600

    assets: add images for click counter UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import logo from './img/banner.jpg'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='logo-container'>
+        <image 
+          className='code-logo'
+          src={logo}
+          alt='logo de fondo'/>
+      </div>
     </div>
   );
 }
