89efecee72a52a74a9a99020bc38e6b2367cfb34
Jsson47 <martinezvalle98@gmail.com>
2025-03-25 09:52:20 -0600

    feat: add increment and reset button handlers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Button from './components/Button';
 import bannerLogo from './img/banner-custom.png'
 
 
 function App() {
+
+  const handleClick = (esClickButton) => {
+    console.log("Click");
+}
+
+  const handleReset = (esClickButton) => {
+    console.log("Reset"); 
+}
+
   return (
     <div className="App">
       <div className='logo-container'>
@@ -12,7 +22,14 @@ function App() {
           alt='logo de fondo'/>
       </div>
       <div className='principal-container'>
-          
+          <Button
+            text="Click"
+            esClickButton={true}
+            operateClick={handleClick}/>
+          <Button
+          text="reset"
+          esClickButton={false}
+          operateClick={handleReset}/>
       </div>
     </div>
   );
