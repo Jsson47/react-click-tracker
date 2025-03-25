3acaeefd85118f3894a10d4065b192079a87995c
Jsson47 <martinezvalle98@gmail.com>
2025-03-24 22:18:09 -0600

    style: add CSS styles for click counter UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import logo from './img/banner.jpg'
+import bannerLogo from './img/banner-custom.png'
 
 
 function App() {
   return (
     <div className="App">
       <div className='logo-container'>
-        <image 
-          className='code-logo'
-          src={logo}
+        <img
+          className='banner-logo'
+          src={bannerLogo}
           alt='logo de fondo'/>
+      </div>
+      <div className='principal-container'>
+        
       </div>
     </div>
   );
