46b7c3559cd975b14113162601750ab87124bc6c
Jsson47 <martinezvalle98@gmail.com>
2025-03-25 09:02:10 -0600

    feat: update buttons with conditional styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function Button({text, isClickButton }) { 
+function Button({text, isClickButton, operateClick }) { 
   return (
     <button
-      className={ isClickButton ? "click-button" : "reset-button" }>
+      className={ isClickButton ? "click-button" : "reset-button" }
+      onClick={operateClick}>
       {text}
     </button>
     );
   
 }
+
+export default Button;
